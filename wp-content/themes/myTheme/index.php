26e83164653e55064d1e8bc9c8d7272ba1abf56d
muham <muhammadaaa727@gmail.com>
2022-11-09 21:06:35 +0300

    Initial commit

---
+++ wp-content/themes/myTheme/index.php
@@ -0,0 +1,21 @@
+<!--
+this is first page and added theme in all pages
+first header
+second sidebar
+third search form
+forth footer
+
+and finish
+-->
+
+<?php get_header(); ?>
+
+<b>Hi in first theme</b>
+
+<?php get_sidebar(); ?>
+
+
+<?php get_search_form(); ?>
+
+
+<?php get_footer(); ?>
