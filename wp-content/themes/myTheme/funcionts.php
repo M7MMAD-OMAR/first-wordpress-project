e9fef22a562d6dc78fd2e91e67ce591a653ad134
muham <muhammadaaa727@gmail.com>
2022-11-10 15:05:08 +0300

    added function get_script_files()

--- wp-content/themes/myTheme/funcionts.php
+++ wp-content/themes/myTheme/funcionts.php
@@ -2,13 +2,10 @@
 
 
 function get_style_files(): void {
-	wp_enqueue_block_style("style-css", (array) (get_template_directory_uri() . 'css/styleCss.css') );
+	wp_enqueue_style("style-css",  get_template_directory_uri() . '/css/styleCss.css');
 }
 
 
-function get_js_files(): void {
-	wp_enqueue_block_style("main-js", (array) (get_template_directory_uri() . 'js/mainJs.js') );
+function get_script_files(): void {
+	wp_enqueue_script("main-js", get_template_directory_uri() . '/js/mainJs.js', array(), false, true);
 }
-
-
-?>
