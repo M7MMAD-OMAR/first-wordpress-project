e71b48dff2f6794beb0f712cdbe024476b958fae
muham <muhammadaaa727@gmail.com>
2022-11-10 14:58:30 +0300

    create function js from function files

--- wp-content/themes/myTheme/funcionts.php
+++ wp-content/themes/myTheme/funcionts.php
@@ -1,8 +1,13 @@
 <?php
 
 
-function style_files(): void {
-	wp_enqueue_block_style("style-css", get_template_directory_uri() . 'css/styleCss.css');
+function get_style_files(): void {
+	wp_enqueue_block_style("style-css", (array) (get_template_directory_uri() . 'css/styleCss.css') );
+}
+
+
+function get_js_files(): void {
+	wp_enqueue_block_style("main-js", (array) (get_template_directory_uri() . 'js/mainJs.js') );
 }
 
 
