c7f239f88c8f5a47a229274eebf3f2a45410df84
muham <muhammadaaa727@gmail.com>
2022-11-13 12:28:00 +0300

    added bootstrap files js, css

--- wp-content/themes/myTheme/functions.php
+++ wp-content/themes/myTheme/functions.php
@@ -7,11 +7,14 @@
 
 function get_style_files(): void {
 	wp_enqueue_style( "style-css", get_template_directory_uri() . '/css/styleCss.css' );
+	wp_enqueue_style("bootstrap-css", get_template_directory_uri() . '/css/bootstrap.min.css');
 }
 
 
 function get_script_files(): void {
 	wp_enqueue_script( "main-js", get_template_directory_uri() . '/js/main.js', array(), false, true );
+	wp_enqueue_script("bootstrap-js", get_template_directory_uri() . '/js/bootstrap.min.js');
+
 }
 
 
