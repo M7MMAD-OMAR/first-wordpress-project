621b99e25ec961f5f985508451e7baf979c44b14
muham <muhammadaaa727@gmail.com>
2022-11-13 12:24:18 +0300

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{php,html,js}","./template-parts/*.{php,html,js}"],
   theme: {
     extend: {},
   },
